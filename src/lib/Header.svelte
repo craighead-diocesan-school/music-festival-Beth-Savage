0b0f884239983457bcdf99032506052bf29385a0
Beth-Savage <145308916+Beth-Savage@users.noreply.github.com>
2025-05-20 15:08:32 +1200

    Created Header
    Created and styled a header element with content, background image, font from google fonts and colours for the text and text box. Header can then be imported into all pages with subtitle  able to be changed on each page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,38 @@
+<script>
+  export let title = "Sunburst Festival";
+  export let subtitle = "Subtitle";
+</script>
 <header class="hero is-primary">
   <div class="hero-body">
-    <h1 class="title">The SvelteKit Template</h1>
-    <h2 class="subtitle">Welcome to the start of your next greatest project.</h2>
+    <h1 class="title">{title}</h1>
+    <h2 class="subtitle"> {subtitle}</h2>
   </div>
 </header>
+
+<style>
+  header {
+    background-image:url(/samuel-regan-asante.jpg);
+    background-repeat: repeat-x;
+    background-size: contain;
+    padding: 5%;
+  }
+  .hero-body {
+    background-color: #8F0A9E;
+    margin-left:25%;
+    margin-right:25%;
+    align-items: center;
+  }
+  .title{
+    color: #FFFFFF;
+    font-size: 4em;
+    text-align: center;
+    font-family: "Trade Winds", system-ui;
+  }
+  .subtitle{
+    color: #F0EAFA;
+    font-size: 2em;
+    text-align: left;
+    margin-left: 15%;
+    font-family: "Trade Winds", system-ui;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
-  import '../app.scss'
-  
-  let { children } = $props()
+  import "../app.scss";
+
+  let { children } = $props();
 </script>
 
 {@render children?.()}
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Trade+Winds&display=swap");
+</style>
