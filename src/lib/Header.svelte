90fe6d0ebb696bd350b5a5b6a0222cd4110ef3c1
Beth-Savage <145308916+Beth-Savage@users.noreply.github.com>
2025-05-22 08:54:02 +1200

    Header update and Format
    Added credits to the photographer of the header background and customized for home page. Also formatted both files

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,37 +2,46 @@
   export let title = "Sunburst Festival";
   export let subtitle = "Subtitle";
 </script>
+
 <header class="hero is-primary">
   <div class="hero-body">
     <h1 class="title">{title}</h1>
-    <h2 class="subtitle"> {subtitle}</h2>
+    <h2 class="subtitle">{subtitle}</h2>
   </div>
+  <p class="credits">&copy Samuel Regan Asante</p>
 </header>
 
 <style>
   header {
-    background-image:url(/samuel-regan-asante.jpg);
+    background-image: url(/samuel-regan-asante.jpg);
     background-repeat: repeat-x;
     background-size: contain;
-    padding: 5%;
+    padding-top: 5%;
   }
   .hero-body {
-    background-color: #8F0A9E;
-    margin-left:25%;
-    margin-right:25%;
+    background-color: #8f0a9e;
+    margin-left: 25%;
+    margin-right: 25%;
+    margin-bottom: 5%;
     align-items: center;
   }
-  .title{
-    color: #FFFFFF;
+  .title {
+    color: #ffffff;
     font-size: 4em;
     text-align: center;
     font-family: "Trade Winds", system-ui;
   }
-  .subtitle{
-    color: #F0EAFA;
+  .subtitle {
+    color: #f0eafa;
     font-size: 2em;
     text-align: left;
     margin-left: 15%;
     font-family: "Trade Winds", system-ui;
+  }
+  .credits {
+    color: #f0eafa;
+    align-self: left;
+    align-self: baseline;
+    font-size: 0.5em;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,29 @@
 <script>
-  import Header from '$lib/Header.svelte'
-  import BulmaExamples from '$lib/BulmaExamples.svelte'
+  import Header from "$lib/Header.svelte";
+  import BulmaExamples from "$lib/BulmaExamples.svelte";
 </script>
 
-<Header />
+<Header subtitle="Home" />
 
 <main class="content section">
   <h2>SvelteKit</h2>
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+  <p>
+    Welcome to coding with SvelteKit, a modern JavaScript framework that makes
+    it easy to code great apps.
+  </p>
 
-  <p>This template comes loaded with the <a href="https://bulma.io/documentation/">Bulma CSS framework</a>, so you can save time and focus on your project.</p>
+  <p>
+    This template comes loaded with the <a
+      href="https://bulma.io/documentation/">Bulma CSS framework</a
+    >, so you can save time and focus on your project.
+  </p>
 
-  <p>Here's some examples of colour helpers. You can change the colours in <code>theme.scss</code></p>
+  <p>
+    Here's some examples of colour helpers. You can change the colours in <code
+      >theme.scss</code
+    >
+  </p>
   <BulmaExamples />
 </main>
 
