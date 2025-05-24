15d5192cd9a5ad2c4724590e39b1b5fc9592d208
Beth-Savage <145308916+Beth-Savage@users.noreply.github.com>
2025-05-25 09:41:21 +1200

    Added content
    Added images and text to the accommodations page with formatting to suit. Also fixed spelling error in subtitle

--- src/routes/accommodation/+page.svelte
+++ src/routes/accommodation/+page.svelte
@@ -1,45 +1,86 @@
 <script>
-    import Header from "$lib/Header.svelte";
-    import Nav from "$lib/Nav.svelte";
-    import Footer from "$lib/Footer.svelte";
-  </script>
-  
-  <Header subtitle="Accomodations" />
-  <Nav />
-  <main class="content section">
-    <img class="column is-half" src="/" alt="" />
-    <div class="text column is-half"></div>
-    <img class="column is-half" src="/" alt="" />
-    <div class="text column is-half"></div>
-  </main>
-  <Footer />
-  
-  <style>
-    .content {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: space-between;
-    }
-    img {
-      width: 50%;
-      height: auto;
-    }
-    .text {
-      margin-top: 5%;
-      margin-bottom: 5%;
-      padding: 1%;
-      background-color: #e5c2dd;
-      align-items: center;
-      opacity: 99%;
-    }
-    p {
-      font-family: "Permanent Marker", cursive;
-      opacity: 100%;
-    }
-    main.content {
-      background-color: #1d2f3e;
-      margin-bottom: 0%;
-    }
-  </style>
-  
+  import Header from "$lib/Header.svelte";
+  import Nav from "$lib/Nav.svelte";
+  import Footer from "$lib/Footer.svelte";
+</script>
+
+<Header subtitle="Accommodations" />
+<Nav />
+<main class="content section">
+  <div class="top text">
+    <p>
+      Experience the ultimate music festival adventure with our tent
+      accommodation packages! With our comfortable and convenient tents, you can
+      immerse yourself in the festival atmosphere and stay close to all the
+      action.
+    </p>
+  </div>
+  <img class="column is-half" src="/krists-luhaers.jpg" alt="" />
+  <div class="text column is-half">
+    <p>
+      Our tents are spacious and come equipped with everything you need for a
+      comfortable stay. With comfortable beds, pillows, and blankets, you can
+      get a good night's sleep and wake up feeling refreshed and ready for
+      another day of music and fun. Located within the festival grounds, our
+      tents are just a short walk away from all the stages and attractions. You
+      won't have to worry about transportation or navigating crowds to get to
+      the festival - you'll be right in the heart of it all.
+    </p>
+  </div>
+  <div class="text column is-half">
+    <p>
+      Our tents also come with a range of amenities to make your stay even more
+      comfortable. You'll have access to clean restroom facilities, hot showers,
+      and charging stations for your devices. We also have a dedicated staff
+      available to assist with any needs or questions you may have during your
+      stay. But the best part of our tent accommodation packages is the
+      community. You'll be surrounded by other festival-goers who are just as
+      excited as you are to be a part of the experience. You'll make new
+      friends, share stories, and create memories that will last a lifetime.
+    </p>
+  </div>
+  <img class="column is-half" src="/vonecia-carswell.jpg" alt="" />
+  <div class=" top text column is-half">
+    <p>
+      Don't miss out on this opportunity to enhance your music festival
+      experience. Book your tent accommodation package today and get ready for
+      an unforgettable adventure. We can't wait to see you there!
+    </p>
+  </div>
+</main>
+<Footer />
+
+<style>
+  .content {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  img {
+    width: 50%;
+    height: auto;
+  }
+  .text {
+    margin-top: 5%;
+    margin-bottom: 5%;
+    padding: 1%;
+    background-color: #e5c2dd;
+    align-items: center;
+    opacity: 99%;
+  }
+  p {
+    font-family: "Permanent Marker", cursive;
+    opacity: 100%;
+  }
+  main.content {
+    background-color: #1d2f3e;
+    margin-bottom: 0%;
+  }
+  .top {
+    align-self: center;
+    margin-left: 25%;
+    margin-right: 25%;
+    font-size: 1.25em;
+  }
+</style>
